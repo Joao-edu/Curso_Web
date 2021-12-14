bfec52bb3f29cdbefdec289fc6bfac9ada667449
Joao Eduardo <joaoeduardo616@yahoo.com>
2021-12-13 21:54:22 -0300

    Let

--- 2-Seção, fundamentos/14-JavaScript_Let.js
+++ 2-Seção, fundamentos/14-JavaScript_Let.js
@@ -1,6 +1,6 @@
 var numero = 1 
   { 
-     let numero = 2
+     let numero = 2 // Variável Let não sobrescreve como aconte com Var pois são escopo diferente
      console.log("Dentro =", numero)
   }
 
