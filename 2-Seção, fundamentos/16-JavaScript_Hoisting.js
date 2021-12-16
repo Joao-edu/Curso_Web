1222ff710c09a6a8ab65c54865b66e6fe604b530
Joao Eduardo <joaoeduardo616@yahoo.com>
2021-12-16 18:17:12 -0300

    Objeto vs Funcao

--- 2-Seção, fundamentos/16-JavaScript_Hoisting.js
+++ 2-Seção, fundamentos/16-JavaScript_Hoisting.js
@@ -1,11 +1,11 @@
 //Tradução da palavra Hoisting = içar jogar para cima
 
-console.log("a =", a)
+console.log("a =", a)//O valor "2" não é atribuido a variável 
 var a = 2
 console.log("a =", a)
 
 function teste(){
-    console.log("b =", b)
+    console.log("b =", b)//Iça a variável "b" porem não o valor atribuido a ela "4"
     var b = 4
     console.log("b =", b)
 }
